0e1d1e58ff275a86e2b331cb21959e750b397bb7
ariessa-deriv <>
2022-07-12 09:42:57 +0800

    Add age and colour

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import "./style.css";
+import "./App.css";
 
 export default function App() {
   const [name, setName] = React.useState<string>("");
-  const [age, setAge] = React.useState<string | undefined>();
+  const [age, setAge] = React.useState<string | undefined>("");
+  const [color, setColor] = React.useState<string | undefined>("");
 
   const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -13,25 +14,47 @@ export default function App() {
     setAge(event.target.value);
   };
 
+  const changeColor = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setColor(event.target.value);
+  };
+
   return (
     <div>
       <Input name="Name" value={name} onChange={changeName} />
       <Input name="Age" value={age} onChange={changeAge} />
+      <Select name="Favourite Color" value={color} onChange={changeColor} />
     </div>
   );
 }
 
 type InputType = {
   name: string;
-  value: string;
+  value: string | undefined;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Input = ({ value, onChange, name }) => {
+const Input = ({ value, onChange, name }: InputType) => {
   return (
-    <div>
+    <div className="pa-16">
       <label>{name}</label>
       <input value={value} onChange={onChange} />
+    </div>
+  );
+};
+
+type SelectType = {
+  name: string;
+  value: string | undefined;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+const Select = ({ value, onChange, name }: SelectType) => {
+  return (
+    <div className="pa-16">
+      <label>{name}</label>
+      <select value={value} onSelect={onChange}>
+        <option value="red">Red</option>
+      </select>
     </div>
   );
 };
